2d845fef9953cee22fda5f64ddec7e1d30369195
fernando <fernandosrojo@yahoo.com>
2017-10-05 15:24:27 -0400

    recommit

--- js/javascript.js
+++ js/javascript.js
@@ -1,71 +1,115 @@
 $(document).ready(function () {
 
-    var $header = $('.mainHeader'),
-        $headerHeight = $header.height(),
-        $meter = $('section').first().height(),
-        $mobileHeader = $('.menu'),
-        $mobileHeaderTrigger = $('.menuTrigger'),
-        $body = $('body');
+            var $header = $('.mainHeader'),
+                $headerHeight = $header.height(),
+                $meter = $('section').first().height(),
+                $mobileHeader = $('.menu'),
+                $mobileHeaderTrigger = $('.menuTrigger'),
+                $body = $('body');
 
-    function headerScroller() {
-        $scrolled = $(window).scrollTop();
-        if ($scrolled > 100) {
-            $header.addClass('active');
-        } else {
-            $header.removeClass('active');
-        }
-    };
+            function headerScroller() {
+                $scrolled = $(window).scrollTop();
+                if ($scrolled > 100) {
+                    $header.addClass('active');
+                } else {
+                    $header.removeClass('active');
+                }
+            };
 
-    function mobileHeader() {
-        $body.toggleClass('menuOpen');
-        $header.toggleClass('hideUp');
-        $body.toggleClass('overflowHidden');
-        window.scrollTo(0, 0);
-    };
+            function mobileHeader() {
+                $body.toggleClass('menuOpen');
+                $header.toggleClass('hideUp');
+                $body.toggleClass('overflowHidden');
+                window.scrollTo(0, 0);
+                if ($body.hasClass('menuOpen')) {
+                    $('.menu').find('li').addClass('fadeInRight animated');
+                } else {
+                    $('.menu').find('li').removeClass('fadeInRight animated');
+                }
+            };
 
-    if (window.location.hash) {
-        var hash = window.location.hash.substring(1), //Puts hash in variable, and removes the # character
-            splitHash = hash.replace('#', ''),
-            $newPage = $("#" + splitHash);
-        if ($newPage.length != 0) {
-            $('page').removeClass('active');
-            $newPage.addClass('active');
-        }
-    };
+            function pageChange(e) { // let's make this cool as fuck
+                var $currentPage = $('page.active'),
+                    $link = $(this).attr('href').replace('.html', ''),
+                    $targetPage = $('#' + $link);
 
-    headerScroller(); // initiate proper header colors based on scroll position
+                // Basic conditions to know the linked object exists, isn't an external link, and isn't currently active.
+                if ($targetPage.length != 0 && $(this).attr('target') != '_blank' && !$targetPage.hasClass('active')) {
+                    e.preventDefault();
+                    $currentPage.addClass('hideFirst');
+                    $currentPage.addClass('hideSecond');
+                };
+            };
 
-    $('a[href$=".html"]').on('click', function (e) {
-        var $link = $(this).attr('href').replace('.html', ''),
-            $newPage = $('#' + $link);
-        if ($newPage.length != 0 && $(this).attr('target') != '_blank') {
-            e.preventDefault();
-            $('page').removeClass('active');
-            $newPage.addClass('active');
-        }
-        window.location.hash = '#' + $link;
-    });
+            if (window.location.hash) {
+                var hash = window.location.hash.substring(1), //Puts hash in variable, and removes the # character
+                    splitHash = hash.replace('#', ''),
+                    $newPage = $("#" + splitHash);
+                if ($newPage.length != 0) {
+                    $('page').removeClass('active');
+                    $newPage.addClass('active');
+                }
+            };
 
-    $(window).scroll(function () { // change proper header colors based on scroll position
-        headerScroller();
-    });
+            headerScroller(); // initiate proper header colors based on scroll position
 
-    $mobileHeaderTrigger.on('click', function () { // show / hide mobile menu
-        mobileHeader();
-    });
+            $('a[href$=".html"]').on('click', function (e) {
 
-    $(window).scroll(function () { // initiate plugin
-        $('.animateMePlz').animateMePlz();
-    });
+                    var $currentPage = $('page.active'),
+                        $link = $(this).attr('href').replace('.html', ''),
+                        $targetPage = $('#' + $link);
+                    if ($targetPage.length != 0) {
+                        e.preventDefault();
+                    }
+                    if ($targetPage.length != 0 && $(this).attr('target') != '_blank' && !$targetPage.hasClass('active')) {
+                        e.preventDefault();
+                        $('page').removeClass('active');
+                        $targetPage.addClass('active');
+                    }
 
+                    //if ($(this).closest('.menu').length == 0) { // if it's on desktop
+                        // Basic conditions to know the linked object exists, isn't an external link, and isn't currently active.
+                        //            if ($targetPage.length != 0 && $(this).attr('target') != '_blank' && !$targetPage.hasClass('active')) {
+                        //                $currentPage.addClass('beingMoved');
+                        //                $targetPage.addClass('beingMoved');
+                        //                $currentPage.addClass('hideFirst'); // zoom the page in
+                        //                $targetPage.addClass('showFirst').delay(750).queue(function () { // slide it over after .75seconds
+                        //                    $currentPage.addClass("hideSecond");
+                        //                    $targetPage.addClass('showSecond').dequeue();
+                        //                }).delay(750).queue(function () { // finish after .75seconds
+                        //                    $targetPage.addClass('showFinal').dequeue();
+                        //                }).delay(750).queue(function () { // finish after .75seconds
+                        //                    $targetPage.addClass('active').removeClass('beingMoved showFirst showSecond showFinal');
+                        //                    $currentPage.removeClass('active beingMoved hideFirst hideSecond').dequeue();
+                        //                }).delay(750).queue(function () { // finish after .75seconds
+                        //                    $currentPage.removeClass('active beingMoved hideFirst hideSecond');
+                        //                    $targetPage.addClass('active').removeClass('beingMoved showFirst showSecond showFinal').dequeue();
+                        //                });
+                        //            };
+                        //        } else { // if it's from the mobile drawer
+                        //  }
+                        window.location.hash = '#' + $link;
+                    });
 
-    $('.pageOverlay').on('click', function () {
-        if ($(this).parent().hasClass('active')) {
-            mobileHeader();
-        }
-    });
+                $(window).scroll(function () { // change proper header colors based on scroll position
+                    headerScroller();
+                });
 
-    $mobileHeader.find('a').on('click', function () {
-        mobileHeader();
-    });
-});
+                $mobileHeaderTrigger.on('click', function () { // show / hide mobile menu
+                    mobileHeader();
+                });
+
+                $(window).scroll(function () { // initiate plugin
+                    $('.animateMePlz').animateMePlz();
+                });
+
+                $('.pageOverlay').on('click', function () {
+                    if ($(this).parent().hasClass('active')) {
+                        mobileHeader();
+                    }
+                });
+
+                $mobileHeader.find('a').on('click', function () {
+                    mobileHeader();
+                });
+            });
