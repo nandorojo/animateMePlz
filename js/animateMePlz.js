b539addec8d8df7d3d8f4239f806c609ad9ca4ad
fernando <fernandosrojo@yahoo.com>
2018-05-26 15:07:52 +0700

    fixes

--- js/animateMePlz.js
+++ js/animateMePlz.js
@@ -7,66 +7,63 @@
             threshold: 50,
             animationStyle: 'fadeIn',
             repeat: 'repeat',
-            animationDelay: '.45s'
+            animationDelay: '.45s',
+            animationDuration: '.5s'
         }, options);
 
-        var $windowHeight = $(window).height(),
-            $scrolled = $(window).scrollTop(),
-            $threshold = settings.threshold,
-            $animationStyle = settings.animationStyle,
-            $repeat = settings.repeat,
-            $animationDelay = settings.animationDelay;
+        var windowHeight = $(window).height(),
+            scrolled = $(window).scrollTop()
 
         this.each(function () {
             var $elementOffset = $(this).offset().top, // distance from div to document top
-                $delayMePlz = $(this).attr('data-thresholdplz'),
-                $animateMePlz = $(this).attr('data-animationstyleplz'),
-                $animationDelayPlz = $(this).attr('data-animationdelayplz'),
-                $repeatPlz = $(this).attr('data-repeatplz'); // attribute for element's scroll threshold
+                $delayMePlz = $(this).data('thresholdplz'),
+                $animateMePlz = $(this).data('animationstyleplz'),
+                $animationDelayPlz = $(this).data('animationdelayplz'),
+                $animationDuration = $(this).data('animationdurationplz'),
+                $repeatPlz = $(this).data('repeatplz'); // attribute for element's scroll threshold
 
             // define scroll delay threshold
             if (typeof $delayMePlz !== typeof undefined && $delayMePlz !== false) { // If given has scroll threshold attribute defined
-                $threshold = parseInt($delayMePlz); // Set delay threshold to the defined pixel amount
-            } else { // TODO is this redundant?
-                $threshold = settings.threshold;
+                settings.threshold = parseInt($delayMePlz); // Set delay threshold to the defined pixel amount
             }
-
             // define animation style
             if (typeof $animateMePlz !== typeof undefined && $animateMePlz !== false) { // If animation style attribute defined
-                $animationStyle = $animateMePlz; // Set animation style to this div's specific attribute
-            } else { // TODO is this redundant?
-                $animationStyle = settings.animationStyle; // Fallback to default or overridden user option from plugin
+                settings.animationStyle = $animateMePlz; // Set animation style to this div's specific attribute
             }
 
-            // define animation style
+            // define animation repitition
             if (typeof $repeatPlz !== typeof undefined && $repeatPlz !== false) { // If animation style attribute defined
-                $repeat = $repeatPlz; // Set animation style to this div's specific attribute
-            } else { // TODO is this redundant?
-                $repeat = settings.repeat; // Fallback to default or overridden user option from plugin
+                settings.repeat = $repeatPlz; // Set animation style to this div's specific attribute
             }
-
             // define animation style
             if (typeof $animationDelayPlz !== typeof undefined && $animationDelayPlz !== false) { // If animation style attribute defined
-                $animationDelay = $animationDelayPlz; // Set animation style to this div's specific attribute
-            } else { // TODO is this redundant?
-                $animationDelay = settings.animationDelay; // Fallback to default or overridden user option from plugin
+                settings.animationDelay = $animationDelayPlz; // Set animation style to this div's specific attribute
+            }
+            // define animation duration
+            if (typeof $animationDuration !== typeof undefined && $animationDuration !== false) { // If animation style attribute defined
+                settings.duration = $animationDelayPlz; // Set animation style to this div's specific attribute
             }
 
             $(this).css({
-                'animation-delay': $animationDelay,
-                '-webkit-animation-delay': $animationDelay
-            })
+                'animation-delay': settings.animationDelay,
+                '-webkit-animation-delay': settings.animationDelay,
+                'opacity': '0',
+                'visibility': 'hidden'
+            });
+
+//            if (settings.repeat != 'noRepeat') {
+                $(this).removeClass('animated ' + settings.animationStyle);
+//            }
 
             // return
-            if ($scrolled + $windowHeight >= $elementOffset - $threshold) {
-                $(this).show();
-                return $(this).addClass('animated ' + $animationStyle);
-            } else {
-                if ($repeat != 'noRepeat') {
-                    return $(this).removeClass('animated ' + $animationStyle);
-                }
+            if (scrolled + windowHeight >= $elementOffset - settings.threshold) {
+                $(this).css({
+                    'opacity': '1',
+                    'visibility': 'visible'
+                });
+                $(this).addClass('animated ' + settings.animationStyle);
+                return;
             }
         });
     };
-
 }(jQuery));
